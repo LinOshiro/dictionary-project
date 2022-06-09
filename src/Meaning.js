1e37d9f8694953c9d6724f822c0a9d117959b88f
LinOshiro <99516179+LinOshiro@users.noreply.github.com>
2022-06-09 18:02:35 -0500

    Updated logo

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,11 +9,10 @@ export default function Meaning(props) {
             {props.meaning.definitions.map(function(definition, index) {
                 return (
                   <div key={index}>
-                   <div className="definition">
-                    {definition.definition}</div>
-                <div className="example">
-                      {definition.example}</div>
-                      <Synonyms synonyms={props.meaning.synonyms} />
+                    <div className="definition">{definition.definition}</div>
+                    <div className="example">{definition.example}</div>
+                    <Synonyms synonyms={props.meaning.synonyms} />
+                    <hr />
                   </div>
                 );
             })}
