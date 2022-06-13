a31f0d406d1c4e3f1bc6be4763f21af7a078b9a6
LinOshiro <99516179+LinOshiro@users.noreply.github.com>
2022-06-13 14:25:52 -0500

    fixed bug

--- src/Photos.js
+++ src/Photos.js
@@ -15,7 +15,8 @@ export default function Photos(props) {
                         target="_blank"
                         rel="noreferrer"
                       >
-                        <img src={photo.src.landscape} className="img-fluid" />
+                              <img src={photo.src.landscape} className="img-fluid"
+                        alt="" />
                       </a>
                     </div>
                   );
